9334388e9d165608360d3da2ce936ad4b2ce9856
Visinho <Elvisvisinho@rocketmail.com>
2023-08-01 16:05:48 +0100

    Created a profile page for users

--- pages/profiles.tsx
+++ pages/profiles.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { NextPageContext } from "next";
 import { getSession } from 'next-auth/react';
+import useCurrentUser from '@/hooks/useCurrentUser';
+import { useRouter } from 'next/router';
 
 export async function getServerSideProps(context: NextPageContext) {
     const session = await getSession(context);
@@ -20,9 +22,27 @@ export async function getServerSideProps(context: NextPageContext) {
 }
 
 const Profiles = () => {
+
+    const { data: user } = useCurrentUser();
+    const router = useRouter();  
+
   return (
-    <div>
-      <p className='text-white text-4xl'>Profiles</p>
+    <div className='flex items-center h-full justify-center'>
+        <div className='flex flex-col'>
+            <h1 className='text-3xl md:text-6xl text-white text-center'>Whose gaze is fixed upon this screen?</h1>
+            <div className='flex items-center justify-center gap-8 mt-10'>
+                <div onClick={() => router.push("/")}>
+                    <div className='group flex-row w-44 mx-auto'>
+                        <div className='w-44 h-44 rounded-md flex items-center border-2 border-transparent group-hover:cursor-pointer group-hover:border-white overflow-hidden'>
+                            <img src='/images/default.jpg' alt='profile' />
+                        </div>
+                        <div className='mt-4 text-gray-400 text-2xl text-center group-hover:text-white'>
+                            <h1>{user?.name}</h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
   )
 };
