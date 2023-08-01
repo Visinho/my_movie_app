c72f22c9baa767ea3f320308b5865ef62fc2d076
Visinho <Elvisvisinho@rocketmail.com>
2023-08-01 03:24:52 +0100

    fixed the prisma schema problem

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -92,11 +92,17 @@ const Auth = () => {
             </button>
 
             <div className="flex flex-row items-center gap-4 mt-8 justify-center">
-              <div onClick={() => signIn("github", {callbackUrl: "/"})} className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-50 transition">
-                <FaGithub size={30}/>
+              <div
+                onClick={() => signIn("github", { callbackUrl: "/" })}
+                className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-50 transition"
+              >
+                <FaGithub size={30} />
               </div>
-              <div className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-50 transition">
-                <FcGoogle size={30}/>
+              <div
+                onClick={() => signIn("google", { callbackUrl: "/" })}
+                className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-50 transition"
+              >
+                <FcGoogle size={30} />
               </div>
             </div>
 
