fde95056b93ac749f895d2b65e4f33531ba81753
Visinho <Elvisvisinho@rocketmail.com>
2023-08-02 18:34:50 +0100

    Styling the Latest Trends section of the page

--- components/MovieList.tsx
+++ components/MovieList.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { isEmpty } from "lodash";
+import MovieCard from './MovieCard';
 
 interface MovieListProps {
     data: Record<string, any>[];
@@ -15,16 +16,14 @@ const MovieList: React.FC<MovieListProps> = ({
     }
 
   return (
-    <div className='px-4 md:px-12 mt-4 space-y-8'>
+    <div className='px-4 md:px-12 mt-4 space-y- 8'>
       <div>
         <p className='text-white text-md md:text-xl lg:text-2xl font-semibold mb-4'>
             {title}
         </p>
         <div className='grid grid-cols-4 gap-2'>
             {data.map((movie) => (
-                <div key={movie.id}>
-                   Movie 
-                </div> 
+                <MovieCard key={movie.id} data={movie}/>
             ))}
         </div>
       </div>
