9f1504d315ecb9890cc75a65d39d62924b2380c5
Visinho <Elvisvisinho@rocketmail.com>
2023-08-22 01:01:05 +0100

    Implemented the play button functionality across relevant pages

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BsFillPlayFill } from 'react-icons/bs';
 import FavoriteButton from './FavoriteButton';
+import { useRouter } from "next/router";
+
 
 interface MovieCardProps {
     data: Record<string, any>;
@@ -9,6 +11,8 @@ interface MovieCardProps {
 const MovieCard: React.FC<MovieCardProps> = ({
     data
 }) => {
+  const router = useRouter();
+
   return (
     <div className='group bg-zinc-900 col-span relative h-[12vw]'>
       <img className='cursor-pointer object-cover transition duration shadow-xl rounded-md group-hover:opacity-90 sm:group-hover:opacity-0 delay-300 w-full h-[12vw]' src={data.thumbnailUrl} alt='thumbnail' />
@@ -17,7 +21,7 @@ const MovieCard: React.FC<MovieCardProps> = ({
         <div className='z-10 bg-zinc-800 p-2 lg:p-4 absolute w-full transition shadow-md rounded-b-md'>
             <div className='flex flex-row items-center gap-2'>
                 <div className='cursor-pointer w-6 h-6 lg:w-10 lg:h-10 bg-white rounded-full flex justify-center items-center transition hover:bg-neurtal-300' onClick={() => {}}>
-                    <BsFillPlayFill size={30} />
+                    <BsFillPlayFill size={30} onClick={() => router.push(`/watch/${data?.id}`)}/>
                 </div>
                 <FavoriteButton movieId={data?.id}/>
             </div>
